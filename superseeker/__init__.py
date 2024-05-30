90686f1981b129aa8e0653fb908ca3c43488e31a
Gage Black <u0829237@redwood9.int.chpc.utah.edu>
2024-05-30 16:35:59 -0600

    update init

--- superseeker/__init__.py
+++ superseeker/__init__.py
@@ -3,7 +3,7 @@
 from .data_manipulation import (
     vcf_to_pyclone_input,
     pyclone_to_vcf,
-    extract_sseeker_stats,
+    identify_evolution,
     make_dot_files
 )
 from .pipeline import run_pipeline
